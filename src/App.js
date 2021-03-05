2ab203f7313a6904f535d3cea644b6f37c01fea8
kollead <kollid33@gmail.com>
2021-03-05 23:58:55 +0900

    user redux

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import ChatPage from './components/ChatPage/ChatPage'
 import LoginPage from './components/LoginPage/LoginPage'
 import RegisterPage from './components/RegisterPage/RegisterPage'
 import firebase from "./firebase"
+import {useDispatch, useSelector} from "react-redux"
+import {setUser } from "./redux/actions/user_action"
 
 function App(props) {
-  let history= useHistory();
+
+  let history = useHistory();
+  let dispatch = useDispatch();
+  const isLoading = useSelector(state => state.user.isLoading);
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged(user=>{
       console.log('user: ', user)
       if(user){
         history.push("/");
+        dispatch(setUser(user))
       }else{
         history.push("/login");
       }
@@ -19,15 +26,23 @@ function App(props) {
     
   }, [])
 
-  return (
-    
-      <Switch>
-        <Route exact path="/" component={ChatPage}/>
-        <Route exact path="/login" component={LoginPage}/>
-        <Route exact path="/register" component={RegisterPage}/>
-      </Switch>
-    
-  );
+  if(isLoading){
+    return 
+    <div>
+      ...loading
+    </div>
+  }else{
+      return (
+      
+        <Switch>
+          <Route exact path="/" component={ChatPage}/>
+          <Route exact path="/login" component={LoginPage}/>
+          <Route exact path="/register" component={RegisterPage}/>
+        </Switch>
+      
+    );
+  }
+  
 }
 
 export default App;
