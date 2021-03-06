86c997941e6a6a2009dfc86ae2e72b69742c34d3
kollead <kollid33@gmail.com>
2021-03-06 23:58:45 +0900

    userPanel

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {IoIosChatboxes} from 'react-icons/io'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Image from 'react-bootstrap/Image'
 
 function UserPanel() {
     return (
@@ -8,6 +10,22 @@ function UserPanel() {
             <h3 style={{color:'white'}}>
                 <IoIosChatboxes/>{" "} Chat App
             </h3>
+            <div style={{ display: 'flex', marginBottom: '1rem'}}>
+                <Image src="" 
+                    style={{width: '30p', height: '30px', marginTop: '3px'}}
+                    roundedCircle/>
+                <Dropdown>
+                    <Dropdown.Toggle
+                        style={{background: 'transparent', border: '0px'}}
+                    id="dropdown-basic">
+                        User Name
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item></Dropdown.Item>
+                        <Dropdown.Item></Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
         </div>
     )
 }
