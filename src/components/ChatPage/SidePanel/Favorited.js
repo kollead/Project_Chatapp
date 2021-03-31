7dea1b991878c42342cd6546fff18fc4b88eefc9
kollead <kollid33@gmail.com>
2021-03-31 23:57:38 +0900

    favorited add and remove

--- src/components/ChatPage/SidePanel/Favorited.js
+++ src/components/ChatPage/SidePanel/Favorited.js
@@ -6,14 +6,40 @@ function Favorited() {
 
     useEffect(() => {
         if(user){
-        addListener(user.uid)}
-    }, [])
+            addListener(user.uid)}
+    }, []);
 
-    const usersRef = firebase.database().ref("users")
-    const user = useSelector(state => state.user.currentUser)
+    const usersRef = firebase.database().ref("users");
+    const user = useSelector(state => state.user.currentUser);
+    const [favoritedChatRooms, setfavoritedChatRooms] = useState([]);
+    const [filteredChatRooms, setfilteredChatRooms] = useState([]);
     const addListener = (userId) => {
         usersRef
             .child(userId)
+            .child("favorited")
+            .on("child_added", DataSnapshot => {
+                const favoritedchatRoom ={id: DataSnapshot.key, ...DataSnapshot.val()}
+                setfavoritedChatRooms([...favoritedChatRooms, favoritedchatRoom])
+            })
+        usersRef
+            .child(usersRef)
+            .child("favorited")
+            .on("child_removed", DataSnapshot => {
+                const chatRoomToRemove = {id: DataSnapshot, ...DataSnapshot.val()}
+                const filteredChatRoom = favoritedChatRooms.filter(chatRoom => {
+                    return chatRoom.id !==chatRoomToRemove.id
+                })
+                setfilteredChatRooms(filteredChatRoom)
+            })
+    }
+
+    const renderFavoritedChatRooms = (favorited) => {
+        favorited.length>0 && 
+        favorited.map(chatRoom=>(
+            <li key={chatRoom.id}>
+                
+            </li>
+        ))
     }
 
     return (
@@ -23,7 +49,7 @@ function Favorited() {
                 FAVORITED (1)
             </span>
             <ul style={{listStyleType: 'none', padding: '0'}}>
-
+                {renderFavoritedChatRooms(favoritedChatRooms)}
             </ul>
         </div>
     )
