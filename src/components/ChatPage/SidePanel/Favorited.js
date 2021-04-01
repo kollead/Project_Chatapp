e09afd409281a968624d5206813a934f689a6ef0
kollead <kollid33@gmail.com>
2021-04-01 23:57:42 +0900

    favorit modify

--- src/components/ChatPage/SidePanel/Favorited.js
+++ src/components/ChatPage/SidePanel/Favorited.js
@@ -1,45 +1,49 @@
-import React, {useEffect, useSelector, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {FaRegSmileBeam} from 'react-icons/fa'
 import firebase from '../../../firebase'
+import {useSelector} from 'react-redux'
 
 function Favorited() {
+
+   
+    const usersRef = firebase.database().ref("users");
+    const user = useSelector(state => state.user.currentUser);
+    const [favoritedChatRooms, setfavoritedChatRooms] = useState([]);
 
     useEffect(() => {
         if(user){
             addListener(user.uid)}
     }, []);
 
-    const usersRef = firebase.database().ref("users");
-    const user = useSelector(state => state.user.currentUser);
-    const [favoritedChatRooms, setfavoritedChatRooms] = useState([]);
-    const [filteredChatRooms, setfilteredChatRooms] = useState([]);
-    const addListener = (userId) => {
+    const addListener = (userId) => {        
         usersRef
             .child(userId)
             .child("favorited")
             .on("child_added", DataSnapshot => {
-                const favoritedchatRoom ={id: DataSnapshot.key, ...DataSnapshot.val()}
-                setfavoritedChatRooms([...favoritedChatRooms, favoritedchatRoom])
-            })
+                const favoritedChatRoom ={id: DataSnapshot.key, ...DataSnapshot.val()}
+                setfavoritedChatRooms(Rooms => {return [...Rooms, favoritedChatRoom]})                
+            });
         usersRef
-            .child(usersRef)
+            .child(userId)
             .child("favorited")
             .on("child_removed", DataSnapshot => {
-                const chatRoomToRemove = {id: DataSnapshot, ...DataSnapshot.val()}
-                const filteredChatRoom = favoritedChatRooms.filter(chatRoom => {
-                    return chatRoom.id !==chatRoomToRemove.id
+                const chatRoomToRemove = {id: DataSnapshot.key, ...DataSnapshot.val()}
+                const filteredChatRooms = favoritedChatRooms.filter(chatRoom =>{
+                    return chatRoom.id !== chatRoomToRemove.id;
                 })
-                setfilteredChatRooms(filteredChatRoom)
-            })
+                setfavoritedChatRooms(filteredChatRooms)
+            })      
+        
+        
     }
 
-    const renderFavoritedChatRooms = (favorited) => {
-        favorited.length>0 && 
-        favorited.map(chatRoom=>(
+    const renderFavoritedChatRooms = () => {
+        console.log("favoritedChatRoom: ",favoritedChatRooms)
+        favoritedChatRooms > 0 && favoritedChatRooms.map(chatRoom => (
             <li key={chatRoom.id}>
-                
-            </li>
-        ))
+                1
+            </li>))
+        
     }
 
     return (
@@ -49,7 +53,7 @@ function Favorited() {
                 FAVORITED (1)
             </span>
             <ul style={{listStyleType: 'none', padding: '0'}}>
-                {renderFavoritedChatRooms(favoritedChatRooms)}
+                {renderFavoritedChatRooms()}
             </ul>
         </div>
     )
